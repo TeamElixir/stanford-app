7cb193170c207059983e36ad1b2f36f616c9a94f
VirajSalaka <viraj.14@cse.mrt.ac.lk>
2018-03-29 14:16:40 +0530

    "sort_results class is added to sort each result"

---
+++ src/main/java/org/elixir/Sort_results.java
@@ -0,0 +1,50 @@
+package org.elixir;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.TreeMap;
+
+public class Sort_results {
+
+    public static void main(String[] args) throws IOException {
+        String filePath = new File("").getAbsolutePath();
+        filePath +="/src/main/resources/sentiment_analysis/legal_cases/results/";
+
+        String[] fileNames = {"negative.csv","positive.csv","neutral.csv"};
+
+        TreeMap<String, String> wordMap = new TreeMap <>();
+
+        for(String fileName : fileNames){
+            Scanner scanner = new Scanner(new File(filePath + fileName));
+
+            while(scanner.hasNext()){
+                String line = scanner.nextLine();
+                String firstWord = line.split(",")[0];
+                wordMap.put(firstWord,line);
+            }
+            scanner.close();
+
+            BufferedWriter out = new BufferedWriter(new FileWriter(filePath+"sorted/"+fileName));
+            for(Map.Entry<String,String> entry : wordMap.entrySet()) {
+                String value = entry.getValue();
+                out.write(value);
+                out.newLine();
+            }
+            out.flush();
+            out.close();
+        }
+
+
+
+
+
+
+    }
+
+
+
+}
