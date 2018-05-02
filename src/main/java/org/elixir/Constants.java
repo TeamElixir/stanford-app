d4b1500333d0c417a1841ca3d6a1da70e5edc235
Menuka Warushavithana <menuka.14@cse.mrt.ac.lk>
2018-05-02 21:49:02 +0530

    fix: update POM

--- src/main/java/org/elixir/Constants.java
+++ src/main/java/org/elixir/Constants.java
@@ -2,5 +2,4 @@ package org.elixir;
 
 public class Constants {
 	public static final String GATE_HOME = "/media/menuka/Data/Ubuntu-Installations/GATE-Developer";
-	public static final String GOOGLE_CLOUD_CREDENTIALS = "/home/menuka/google-cse-credentials.json";
 }
