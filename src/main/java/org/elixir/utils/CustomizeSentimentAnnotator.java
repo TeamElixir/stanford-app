df6c36fa4d53b108f12a33bc57e3527792cb73b7
VirajSalaka <viraj.14@cse.mrt.ac.lk>
2018-05-30 09:37:09 +0530

    added new functions to the CustomizedSentimentAnnotator

--- src/main/java/org/elixir/utils/CustomizeSentimentAnnotator.java
+++ src/main/java/org/elixir/utils/CustomizeSentimentAnnotator.java
@@ -1,10 +1,18 @@
 package org.elixir.utils;
 
+import edu.stanford.nlp.ling.CoreAnnotations;
+import edu.stanford.nlp.ling.CoreLabel;
+import edu.stanford.nlp.pipeline.Annotation;
+import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+import edu.stanford.nlp.sentiment.SentimentCoreAnnotations;
 import edu.stanford.nlp.sentiment.SentimentCostAndGradient;
+import edu.stanford.nlp.util.CoreMap;
 import org.elixir.models.PosTaggedWord;
 import org.elixir.models.Sentence;
 
 import java.io.File;
+import java.util.List;
+import java.util.Properties;
 import java.util.Scanner;
 
 public class CustomizeSentimentAnnotator {
@@ -51,5 +59,50 @@ public class CustomizeSentimentAnnotator {
             SentimentCostAndGradient.addPosTagsOfWords(posTaggedWord.getWord(),posTaggedWord.getPosTag());
         }
 
+    }
+
+
+    // if the sentences are not in database: use this method
+    public static void createPosTagMapForSentence(String sentence){
+
+        Properties props = new Properties();
+        props.setProperty("annotators", "tokenize,ssplit,pos");
+        StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
+
+        Annotation annotation = new Annotation(sentence);
+        pipeline.annotate(annotation);
+
+        List<CoreMap> sentences = annotation.get(CoreAnnotations.SentencesAnnotation.class);
+
+        for (CoreMap coreMapSentence : sentences){
+            for (CoreLabel token : coreMapSentence.get(CoreAnnotations.TokensAnnotation.class)){
+
+                String word = token.get(CoreAnnotations.TextAnnotation.class);
+                String posTag = token.get(CoreAnnotations.PartOfSpeechAnnotation.class);
+                SentimentCostAndGradient.addPosTagsOfWords(word,posTag);
+
+            }
+        }
+    }
+
+    /*
+    * -1 for negative, 0 for neutral and 1 for positive
+    */
+    public static int getPhraseSentiment(CoreMap coreMapSentence){
+        String sentiment = coreMapSentence.get(SentimentCoreAnnotations.SentimentClass.class);
+
+        switch(sentiment){
+            case "Negative":
+                return -1;
+
+            case "Neutral" :
+                return 0 ;
+
+            case "Positive" :
+                return 1;
+
+            default :
+                return 0;
+        }
     }
 }
