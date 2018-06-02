59ef534afe753b67a7ee2f112eac9d5f5059e1e8
VirajSalaka <viraj.14@cse.mrt.ac.lk>
2018-06-02 19:04:21 +0530

    find ith occurance code is working, others are not complete - intermediate commit

--- src/main/java/org/elixir/Party_Extraction_Main.java
+++ src/main/java/org/elixir/Party_Extraction_Main.java
@@ -10,12 +10,16 @@ package org.elixir;
 * solve coreferences in sentences and save in the database, along with that it may require to store mentions
 */
 
+import edu.stanford.nlp.io.IOUtils;
 import edu.stanford.nlp.ling.CoreAnnotations;
 import edu.stanford.nlp.neural.rnn.RNNCoreAnnotations;
 import edu.stanford.nlp.pipeline.Annotation;
 import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+import edu.stanford.nlp.semgraph.SemanticGraph;
+import edu.stanford.nlp.semgraph.SemanticGraphCoreAnnotations;
 import edu.stanford.nlp.sentiment.SentimentCoreAnnotations;
 import edu.stanford.nlp.trees.Tree;
+import edu.stanford.nlp.trees.TypedDependency;
 import edu.stanford.nlp.util.CoreMap;
 import org.ejml.simple.SimpleMatrix;
 import org.elixir.utils.CoreNLPDepParser;
@@ -29,19 +33,32 @@ import java.util.Properties;
 *design algorithm;
 */
 
-/*
-milestone : identify the subject of the inner sentence
- */
+
 public class Party_Extraction_Main {
 
     public static void main(String[] args) throws FileNotFoundException {
 
-//        Properties props = new Properties();
-//        props.setProperty("annotators", "tokenize,ssplit,pos,lemma,parse,natlog,depparse,sentiment");
-//        StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
-//
-        String text = " Lee's counsel had performed deficiently by giving improper advice about the deportation consequences of the plea.";
-//
+        /*Properties props = new Properties();
+        props.setProperty("annotators", "tokenize,ssplit,pos,lemma,parse,natlog,depparse,sentiment");
+        StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
+
+        String text = "Lee cannot show prejudice from accepting a plea where his only hope at trial was that something unexpected and unpredictable might occur that would lead to acquittal.";
+
+        Annotation ann = new Annotation(text);
+        pipeline.annotate(ann);
+        for (CoreMap sent : ann.get(CoreAnnotations.SentencesAnnotation.class)) {
+            SemanticGraph sg = sent.get(SemanticGraphCoreAnnotations.BasicDependenciesAnnotation.class);
+            System.out.println(IOUtils.eolChar + sg.toString(SemanticGraph.OutputFormat.LIST));
+
+            for (TypedDependency i : sg.typedDependencies()){
+                System.out.println(i.dep().index() + " : " + i.dep());
+            }
+        }*/
+
+
+
+        //CoreNLPDepParser.depParseForGivenRelation()
+
 //        CustomizeSentimentAnnotator.addSentimentLayerToCoreNLPSentiment("sentimentAnnotator/DeviatedSentimentWords/non_positive_mini.csv",
 //                "sentimentAnnotator/DeviatedSentimentWords/non_negative_mini.csv",
 //                "sentimentAnnotator/DeviatedSentimentWords/non_neutral_mini.csv");
@@ -62,8 +79,50 @@ public class Party_Extraction_Main {
 //
 //        }
 
-        CoreNLPDepParser.depParse(text);
-
-
     }
+
+
+    //should provide the part of sentence begins after the term "that"
+    public static String[] endExtractedInnerSentence(int startIndex , String text){
+        //todo : if that followed by when, where or although kind a word dismiss for now
+        String[] endPointChars = {",", ".", "?", "\\n"};
+
+        int firstEndPointIndex = -1;
+
+        for(String endPoint : endPointChars){
+            if(firstEndPointIndex < text.indexOf(endPoint)){
+                firstEndPointIndex = text.indexOf(endPoint);
+            }
+        }
+        String[] returnString = {null,null};
+        if(firstEndPointIndex > -1){
+            returnString[0] = text.substring(startIndex,firstEndPointIndex);
+            if(firstEndPointIndex < text.length()-1){
+                returnString[1] = text.substring(firstEndPointIndex+1);
+            }
+        }
+        return returnString;
+    }
+
+    //returns the start of inner sentence, should provide the index of that: verb related with the verb prior to that, and whole sentence
+    public static int startPointIndexInnerSentence(int indexOfThat,String verb, String sentence){
+
+        int verbIndex = sentence.indexOf(verb);
+        char[] startChars = {','};
+        int startIndex1 = indexOfThat+3;
+
+        for(int i=verbIndex;i<0;i--){
+            if (startChars[0] == sentence.charAt(i)){
+                if(startIndex1<i){
+                    startIndex1 = i;
+                    break;
+                }
+            }
+        }
+
+        return startIndex1;
+    }
+
+
+
 }
--- src/main/java/org/elixir/utils/CoreNLPDepParser.java
+++ src/main/java/org/elixir/utils/CoreNLPDepParser.java
@@ -1,14 +1,20 @@
 package org.elixir.utils;
 
+import edu.stanford.nlp.ling.CoreAnnotations;
 import edu.stanford.nlp.ling.HasWord;
 import edu.stanford.nlp.ling.TaggedWord;
 import edu.stanford.nlp.parser.nndep.DependencyParser;
+import edu.stanford.nlp.pipeline.Annotation;
 import edu.stanford.nlp.process.DocumentPreprocessor;
+import edu.stanford.nlp.semgraph.SemanticGraph;
+import edu.stanford.nlp.semgraph.SemanticGraphCoreAnnotations;
 import edu.stanford.nlp.tagger.maxent.MaxentTagger;
 import edu.stanford.nlp.trees.GrammaticalStructure;
 import edu.stanford.nlp.trees.TypedDependency;
+import edu.stanford.nlp.util.CoreMap;
 
 import java.io.StringReader;
+import java.util.ArrayList;
 import java.util.List;
 
 public class CoreNLPDepParser {
@@ -33,4 +39,82 @@ public class CoreNLPDepParser {
         }
         return "finished - testing ";
     }
+
+    //to find first that related verb.
+    public static String depParseForGivenRelation(String dependency, String outerVerb, String text){
+        String modelPath = DependencyParser.DEFAULT_MODEL;
+        String taggerPath = "edu/stanford/nlp/models/pos-tagger/english-left3words/english-left3words-distsim.tagger";
+
+        MaxentTagger tagger = new MaxentTagger(taggerPath);
+        DependencyParser parser = DependencyParser.loadFromModelFile(modelPath);
+
+        DocumentPreprocessor tokenizer = new DocumentPreprocessor(new StringReader(text));
+        GrammaticalStructure gs =  null;
+        for (List<HasWord> sentence : tokenizer) {
+            List<TaggedWord> tagged = tagger.tagSentence(sentence);
+            gs = parser.predict(tagged);
+
+        }
+
+        for(TypedDependency typedDependency : gs.typedDependenciesCCprocessed()){
+            if(typedDependency.reln().equals(dependency) && typedDependency.dep().equals(outerVerb)){
+                return typedDependency.gov().toString();
+            }
+        }
+        return null;
+
+    }
+
+    public static ArrayList depParseForCComp(Annotation ann, String text){
+
+        ArrayList<TypedDependency> ccompList = new ArrayList<>();
+        ArrayList<TypedDependency> dependencyList = new ArrayList<>();
+        ArrayList<Integer> occurancesOfThat = new ArrayList<>();
+
+        for (CoreMap sent : ann.get(CoreAnnotations.SentencesAnnotation.class)) {
+
+            SemanticGraph sg = sent.get(SemanticGraphCoreAnnotations.BasicDependenciesAnnotation.class);
+
+            for(TypedDependency typedDependency : sg.typedDependencies()){
+
+                if(typedDependency.dep().toString().equals("that")){
+
+                    if (!occurancesOfThat.contains(typedDependency.dep().index())){
+                        occurancesOfThat.add(typedDependency.dep().index());
+                    }
+
+                }else if(typedDependency.gov().toString().equals("that")){
+
+                    if(!occurancesOfThat.contains(typedDependency.gov().index())){
+                        occurancesOfThat.add(typedDependency.gov().index());
+                    }
+
+                }
+
+                if(typedDependency.reln().equals("ccomp")){
+                    ccompList.add(typedDependency);
+                }
+                else if(typedDependency.reln().equals("mark") && typedDependency.gov().toString().equals("that")){
+                    dependencyList.add(typedDependency);
+                }
+            }
+        }
+        return null;
+
+    }
+
+    //can find ith occurance of a certain word in a sentence : working
+    public static int findIthOccuranceOfWord(String word, String sentence, int occurance){
+        if(occurance <= 0){
+            return -1;
+        }
+        else if(occurance == 1){
+            return sentence.indexOf(word);
+        }
+        else{
+            return sentence.indexOf(word,findIthOccuranceOfWord(word,sentence,occurance-1)+1);
+        }
+    }
+
+
 }
