c5a09cbc740c04e753ab67e7a41987d896b6976c
VirajSalaka <viraj.14@cse.mrt.ac.lk>
2018-06-16 00:01:44 +0530

    corefchain string process part is completed

--- src/main/java/org/elixir/CorefRunner.java
+++ src/main/java/org/elixir/CorefRunner.java
@@ -17,7 +17,7 @@ import java.util.ArrayList;
 
 public class CorefRunner {
     public static void main(String[] args) {
-        int caseNumber = 12;
+        int caseNumber = 11;
         Case aCase = CasesController.getCaseByNumber(caseNumber);
         System.out.println("Case: " + caseNumber);
         ArrayList<Paragraph> paragraphs = aCase.getParagraphs();
@@ -34,7 +34,9 @@ public class CorefRunner {
                 System.out.println("No coref chains!");
             }
             for (CorefChainOfParagraph corefChain : corefChains) {
-                System.out.println(corefChain.getCorefChain());
+                CorefChainMapping ccm = new CorefChainMapping();
+                ccm.processCorefChainString(corefChain.getCorefChain().toString());
+                //System.out.println(corefChain.getCorefChain());
             }
 
             System.out.println("--------\n");
@@ -96,5 +98,46 @@ public class CorefRunner {
         }
 
         return fullCases;
+    }
+
+    //this denotes a single corefchain
+    static class CorefChainMapping{
+        static class CorefSentenceMapping{
+            int sentenceNumber;
+            String term;
+
+        }
+
+        ArrayList <CorefSentenceMapping> termList = new ArrayList<>();
+        String longestTerm;
+
+        void assignTerm(String term, int sentenceNumber){
+
+            CorefSentenceMapping csm = new CorefSentenceMapping();
+            csm.term = term;
+            csm.sentenceNumber = sentenceNumber;
+            termList.add(csm);
+
+            if(termList.size() == 0){
+                longestTerm = term;
+            }else if(term.length() >= longestTerm.length()){
+                longestTerm = term;
+            }
+        }
+
+        //needs to process this kind a string
+        //CHAIN50-["the defendant" in sentence 1, "his" in sentence 1, "the defendant" in sentence 5, "he" in sentence 5]
+        void processCorefChainString(String corefChainString){
+
+            String BracketsRemovedString = corefChainString.substring(corefChainString.indexOf("[")+1,corefChainString.indexOf("]")+1);
+            String[] commaSeperatedReferences = BracketsRemovedString.split(",");
+
+            for(String reference : commaSeperatedReferences){
+                int sentenceNumber = Integer.parseInt(reference.substring(reference.indexOf("in sentence")+12));
+                String term = reference.substring(1,reference.indexOf("in sentence")-2);
+                assignTerm(term,sentenceNumber);
+            }
+        }
+
     }
 }
