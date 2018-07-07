235ed7a6db29a01f4aeb33b2c6d31c0c17512c7d
VirajSalaka <viraj.14@cse.mrt.ac.lk>
2018-07-07 12:37:56 +0530

    modified parse tree demo

--- src/main/java/org/elixir/parseTree/ParseTreeDemo.java
+++ src/main/java/org/elixir/parseTree/ParseTreeDemo.java
@@ -9,7 +9,9 @@ import edu.stanford.nlp.trees.LabeledScoredConstituentFactory;
 import edu.stanford.nlp.trees.Tree;
 import edu.stanford.nlp.trees.TreeCoreAnnotations;
 import edu.stanford.nlp.util.CoreMap;
+import org.elixir.utils.CustomizeSentimentAnnotator;
 
+import java.io.FileNotFoundException;
 import java.util.List;
 import java.util.Properties;
 import java.util.Set;
@@ -19,7 +21,7 @@ public class ParseTreeDemo {
     public static void main(String[] args) {
         // set up pipeline properties
         Properties props = new Properties();
-        props.setProperty("annotators", "tokenize,ssplit,pos,lemma,ner,parse");
+        props.setProperty("annotators", "tokenize,ssplit,pos,lemma,ner,parse,natlog,sentiment");
 
         // use faster shift reduce parser
         props.setProperty("parse.model", "edu/stanford/nlp/models/srparser/englishSR.ser.gz");
@@ -27,6 +29,15 @@ public class ParseTreeDemo {
 
         // set up Stanford CoreNLP pipeline
         StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
+
+        try {
+            CustomizeSentimentAnnotator.addSentimentLayerToCoreNLPSentiment("sentimentAnnotator/DeviatedSentimentWords/non_positive_mini.csv",
+                    "sentimentAnnotator/DeviatedSentimentWords/non_negative_mini.csv",
+                    "sentimentAnnotator/DeviatedSentimentWords/non_neutral_mini.csv");
+        } catch (FileNotFoundException e) {
+            System.out.println("sentiment file is not found.");
+        }
+
 
         // build annotation for a review
         Annotation annotation = new Annotation("The small red car turned very quickly around the corner.");
@@ -36,6 +47,8 @@ public class ParseTreeDemo {
 
         List<CoreMap> sentences = annotation.get(CoreAnnotations.SentencesAnnotation.class);
         for (CoreMap coreMapSentence : sentences) {
+            System.out.println(coreMapSentence.toString());
+            CustomizeSentimentAnnotator.createPosTagMapForSentence(coreMapSentence.toString());
             final String sentiment = coreMapSentence.get(SentimentCoreAnnotations.SentimentClass.class);
             System.out.println("sentiment: " + sentiment);
         }
